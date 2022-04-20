631b9f25ae81daec68ee45d014d685e0e3c05063
Shane Grim <grim8020@gmail.com>
2022-04-20 17:07:21 -0400

    added Xcode

---
+++ First Project/First Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First Project
+//
+//  Created by Shane Grim on 4/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
