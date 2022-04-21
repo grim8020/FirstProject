dc38c9e3fb9f34d20c890523e636c65a81be3132
Shane Grim <grim8020@gmail.com>
2022-04-20 20:03:36 -0400

    updated storyboard

--- First Project/First Project Storyboard/First Project Storyboard/ViewController.swift
+++ First Project/First Project Storyboard/First Project Storyboard/ViewController.swift
@@ -17,11 +17,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         label.text = "Hello Everyone!"
     }
+    
+    //text field is ready to receive input as soon as the app loads - keyboard will show up immediately
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        textField.becomeFirstResponder()
+    }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         let name = textField.text ?? ""
         label.text = "Hi \(name)!"
+        //below code gets rid of the keyboard once the bottom is clicked.
+        textField.resignFirstResponder()
     }
     
+    //when tapped outside of the text field or keyboard, the keyboard disappears
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        textField.resignFirstResponder()
+    }
 }
-
