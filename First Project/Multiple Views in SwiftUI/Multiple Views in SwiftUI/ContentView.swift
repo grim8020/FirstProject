6cfea48344d7dfc7993ee68bd2c93b8f9a25d8bf
Shane Grim <grim8020@gmail.com>
2022-04-21 12:52:06 -0400

    Added Popover

--- First Project/Multiple Views in SwiftUI/Multiple Views in SwiftUI/ContentView.swift
+++ First Project/Multiple Views in SwiftUI/Multiple Views in SwiftUI/ContentView.swift
@@ -15,21 +15,20 @@ struct ContentView: View {
             
             //gives the nav view a title
             
-            
             //create a list, get the data from the DataModel file that we created, and for the id use the id field that we created.
             List(DataModel.data, id: \.self) { object in
                 //block in the curly braces where we can reference our object (WeatherData object) - it will take the data and go through it one by one
                 
                 
-                
                 HStack {
-                    
+                
                     Image(systemName: object.icon)
                     Text("\(object.high)º")
                         .foregroundColor(.red)
                     Text("\(object.low)º")
                         .foregroundColor(.blue)
-                    Text(object.day)
+                   //makes it that you can click on each individual list item for a more detailed view in another window with a back button
+                    NavigationLink(object.day, destination: DetailView(data: object))
                 }
                 .navigationTitle("Spring Grove")
             }
---
+++ First Project/Multiple Views in SwiftUI/Multiple Views in SwiftUI/DetailView.swift
@@ -0,0 +1,41 @@
+//
+//  DetailView.swift
+//  Multiple Views in SwiftUI
+//
+//  Created by Shane Grim on 4/21/22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    var data: WeatherData
+    @State var isPopoverShowing = false
+    
+    var body: some View {
+        VStack {
+            Text(data.day)
+                .padding(0.5)
+            Text("H \(data.high)ºF  L \(data.low)ºF")
+                .padding(0.5)
+            Image(systemName: data.icon)
+                .padding(0.5)
+            Button("Show Popover") {
+                isPopoverShowing = true
+            }
+            .padding()
+            .sheet(isPresented: $isPopoverShowing) {
+                Text("H \(data.high)ºF  L \(data.low)ºF")
+                Image(systemName: data.icon)
+                    .resizable()
+                    .frame(width: 100, height: 100, alignment: .center)
+            }
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(data: DataModel.data[0])
+    }
+}
